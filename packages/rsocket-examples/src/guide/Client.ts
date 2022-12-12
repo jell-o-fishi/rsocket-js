f2675be95fbcd42d93753f81b23a45ef9bba78ff
jell-o-fishi <jellofishi@pm.me>
2022-12-12 18:25:11 +0200

    refactored guide client to a class

--- packages/rsocket-examples/src/guide/Client.ts
+++ packages/rsocket-examples/src/guide/Client.ts
@@ -58,100 +58,12 @@ function createRoute(route?: string) {
   return compositeMetaData;
 }
 
-async function requestResponse(
-  rsocket: RSocket,
-  route?: string,
-  data?: string
-) {
-  return new Promise((resolve, reject) => {
-    return rsocket.requestResponse(
-      {
-        data: Buffer.from(data),
-        metadata: createRoute(route),
-      },
-      {
-        onError: (e) => {
-          reject(e);
-        },
-        onNext: (payload, isComplete) => {
-          Logger.info(
-            `${payload.data}`
-          );
-          resolve(payload);
-        },
-        onComplete: () => {
-          resolve({});
-        },
-        onExtension: () => {
-        },
-      }
-    );
-  });
-}
-
-async function fireAndForget(rsocket: RSocket, route?: string, data?: string) {
-  return new Promise((resolve, reject) => {
-    return rsocket.fireAndForget(
-      {
-        data: Buffer.from(data),
-        metadata: createRoute(route),
-      },
-      {
-        onError: (error) => {
-          reject(error);
-        },
-        onComplete: () => {
-          resolve(null);
-        },
-      }
-    );
-  });
-}
 
 interface StreamControl {
   requester: Requestable & Cancellable & OnExtensionSubscriber
   cancel: () => void
 }
 
-function listenForMessages(rsocket: RSocket): StreamControl {
-  let result = {
-    requester: undefined,
-    cancel: undefined
-  }
-  const promise = new Promise((resolve, reject) => {
-    result.requester = rsocket.requestStream(
-      {
-        data: Buffer.from(""),
-        metadata: createRoute("messages.incoming"),
-      },
-      MAX_REQUEST_COUNT,
-      {
-        onError: (e) => reject(e),
-        onNext: (payload, isComplete) => {
-          Logger.info(
-            `${payload.data}`
-          );
-
-          if (isComplete) {
-            resolve(null);
-          }
-        },
-        onComplete: () => {
-          resolve(null);
-        },
-        onExtension: () => {
-        },
-      }
-    );
-    result.cancel = async function () {
-      result.requester.cancel();
-      resolve(null);
-      await promise;
-    }
-  });
-
-  return result;
-}
 
 interface StatisticsControl {
   requester: OnTerminalSubscriber &
@@ -164,123 +76,218 @@ interface StatisticsControl {
   setPeriodSeconds: (seconds: number) => void
 }
 
-function listenForStatistics(rsocket: RSocket, route?: string, data?: string): StatisticsControl {
-  const result: StatisticsControl = {
-    requester: undefined,
-    cancel: undefined,
-    setStatisticIds: undefined,
-    setPeriodSeconds: undefined
-  };
-  const promise = new Promise((resolve, reject) => {
-    result.requester = rsocket.requestChannel(
-      {
-        data: data == undefined ? undefined : Buffer.from(data),
-        metadata: createRoute("statistics"),
-      },
-      MAX_REQUEST_COUNT,
-      false,
-      {
-        onError: (e) => reject(e),
-        onNext: (payload, isComplete) => {
-          console.log(
-            `${payload.data}`
-          );
 
-          result.requester.request(1);
 
-          if (isComplete) {
+class ChatClient {
+  constructor(private rsocket:RSocket) {
+  }
+
+  public async requestResponse(
+    this,
+    route?: string,
+    data?: string
+  ) {
+    return new Promise((resolve, reject) => {
+      return this.rsocket.requestResponse(
+        {
+          data: Buffer.from(data),
+          metadata: createRoute(route),
+        },
+        {
+          onError: (e) => {
+            reject(e);
+          },
+          onNext: (payload, isComplete) => {
+            Logger.info(
+              `${payload.data}`
+            );
             resolve(payload);
-          }
+          },
+          onComplete: () => {
+            resolve({});
+          },
+          onExtension: () => {
+          },
+        }
+      );
+    });
+  }
+
+  public async fireAndForget(this, route?: string, data?: string) {
+    return new Promise((resolve, reject) => {
+      return this.rsocket.fireAndForget(
+        {
+          data: Buffer.from(data),
+          metadata: createRoute(route),
         },
-        onComplete: () => {
-          resolve(null);
-        },
-        onExtension: () => {
-        },
-        request: (n) => {
-          console.log(`request(${n})`);
-          result.requester.onNext(
-            {
-              data: Buffer.from("Message"),
-            },
-            true
-          );
-        },
-        cancel: () => {
-        },
-      }
-    );
-    result.cancel = async function () {
-      result.requester.cancel();
-      resolve(null);
-      await promise;
+        {
+          onError: (error) => {
+            reject(error);
+          },
+          onComplete: () => {
+            resolve(null);
+          },
+        }
+      );
+    });
+  }
+
+  public listenForMessages(this): StreamControl {
+    let result = {
+      requester: undefined,
+      cancel: undefined
     }
-  });
-
-  result.setStatisticIds = function(ids:string[]) {
-    result.requester.onNext({
-      data: Buffer.from(JSON.stringify({ids:ids}))
-    }, false);
-  }
-
-  result.setPeriodSeconds = function(seconds:number) {
-    result.requester.onNext({
-      data: Buffer.from(JSON.stringify({period_seconds: seconds}))
-    }, false);
-  }
-
-  return result;
-}
-
-async function listValues(rsocket: RSocket, route?: string, data?: string) {
-  return new Promise<string[]>((resolve, reject) => {
-    const values: string[] = [];
-    rsocket.requestStream(
-      {
-        data: data == undefined ? undefined : Buffer.from(data),
-        metadata: createRoute(route),
-      },
-      MAX_REQUEST_COUNT,
-      {
-        onError: (e) => reject(e),
-        onNext: (payload, isComplete) => {
-          values.push(payload.data.toString());
-
-          if (isComplete) {
-            resolve(values);
-          }
+    const promise = new Promise((resolve, reject) => {
+      result.requester = this.rsocket.requestStream(
+        {
+          data: Buffer.from(""),
+          metadata: createRoute("messages.incoming"),
         },
-        onComplete: () => {
-          resolve(values);
-        },
-        onExtension: () => {
-        },
+        MAX_REQUEST_COUNT,
+        {
+          onError: (e) => reject(e),
+          onNext: (payload, isComplete) => {
+            Logger.info(
+              `${payload.data}`
+            );
+
+            if (isComplete) {
+              resolve(null);
+            }
+          },
+          onComplete: () => {
+            resolve(null);
+          },
+          onExtension: () => {
+          },
+        }
+      );
+      result.cancel = async function () {
+        result.requester.cancel();
+        resolve(null);
+        await promise;
       }
-    );
-  });
+    });
+
+    return result;
+  }
+
+  public listenForStatistics(this, route?: string, data?: string): StatisticsControl {
+    const result: StatisticsControl = {
+      requester: undefined,
+      cancel: undefined,
+      setStatisticIds: undefined,
+      setPeriodSeconds: undefined
+    };
+    const promise = new Promise((resolve, reject) => {
+      result.requester = this.rsocket.requestChannel(
+        {
+          data: data == undefined ? undefined : Buffer.from(data),
+          metadata: createRoute("statistics"),
+        },
+        MAX_REQUEST_COUNT,
+        false,
+        {
+          onError: (e) => reject(e),
+          onNext: (payload, isComplete) => {
+            console.log(
+              `${payload.data}`
+            );
+
+            result.requester.request(1);
+
+            if (isComplete) {
+              resolve(payload);
+            }
+          },
+          onComplete: () => {
+            resolve(null);
+          },
+          onExtension: () => {
+          },
+          request: (n) => {
+            console.log(`request(${n})`);
+            result.requester.onNext(
+              {
+                data: Buffer.from("Message"),
+              },
+              true
+            );
+          },
+          cancel: () => {
+          },
+        }
+      );
+      result.cancel = async function () {
+        result.requester.cancel();
+        resolve(null);
+        await promise;
+      }
+    });
+
+    result.setStatisticIds = function(ids:string[]) {
+      result.requester.onNext({
+        data: Buffer.from(JSON.stringify({ids:ids}))
+      }, false);
+    }
+
+    result.setPeriodSeconds = function(seconds:number) {
+      result.requester.onNext({
+        data: Buffer.from(JSON.stringify({period_seconds: seconds}))
+      }, false);
+    }
+
+    return result;
+  }
+
+  public async listValues(this, route?: string, data?: string) {
+    return new Promise<string[]>((resolve, reject) => {
+      const values: string[] = [];
+      this.rsocket.requestStream(
+        {
+          data: data == undefined ? undefined : Buffer.from(data),
+          metadata: createRoute(route),
+        },
+        MAX_REQUEST_COUNT,
+        {
+          onError: (e) => reject(e),
+          onNext: (payload, isComplete) => {
+            values.push(payload.data.toString());
+
+            if (isComplete) {
+              resolve(values);
+            }
+          },
+          onComplete: () => {
+            resolve(values);
+          },
+          onExtension: () => {
+          },
+        }
+      );
+    });
+  }
 }
 
-async function messagesExamples(client1: RSocket, client2: RSocket) {
+async function messagesExamples(client1: ChatClient, client2: ChatClient) {
 
-  const listener1 = listenForMessages(client1);
-  const listener2 = listenForMessages(client2);
+  const listener1 = client1.listenForMessages();
+  const listener2 = client2.listenForMessages();
 
-  await requestResponse(
-    client1,
+  await client1.requestResponse(
     "message",
     '{"user":"user2", "content":"a message"}'
   );
 
-  await requestResponse(client1, "channel.join", "channel1");
-  await requestResponse(client2, "channel.join", "channel1");
+  await client1.requestResponse("channel.join", "channel1");
+  await client2.requestResponse("channel.join", "channel1");
 
-  await requestResponse(
-    client1,
+  await client1.requestResponse(
     "message",
     JSON.stringify({channel: "channel1", content: "a channel message"})
   );
 
-  const channels = await listValues(client1, "channels");
+  const channels = await client1.listValues("channels");
   Logger.info(`channels ${channels}`);
 
   await sleep(1000);
@@ -288,19 +295,18 @@ async function messagesExamples(client1: RSocket, client2: RSocket) {
   await listener2.cancel();
 }
 
-async function filesExample(client1: RSocket) {
-  const files = await listValues(client1, "files");
+async function filesExample(client1: ChatClient) {
+  const files = await client1.listValues("files");
   Logger.info(`files ${files}`);
 }
 
-async function statisticsExample(client1: RSocket) {
-  await fireAndForget(
-    client1,
+async function statisticsExample(client1: ChatClient) {
+  await client1.fireAndForget(
     "statistics",
     JSON.stringify({memory_usage: 123})
   );
 
-  const control = listenForStatistics(client1, "statistics");
+  const control = client1.listenForStatistics("statistics");
 
   await sleep(5000);
   control.setStatisticIds(['users']);
@@ -314,11 +320,11 @@ async function main() {
   const connector1 = makeConnector();
   const connector2 = makeConnector();
 
-  const client1 = await connector1.connect();
-  const client2 = await connector2.connect();
+  const client1 = new ChatClient(await connector1.connect());
+  const client2 = new ChatClient(await connector2.connect());
 
-  await requestResponse(client1, "login", "user1");
-  await requestResponse(client2, "login", "user2");
+  await client1.requestResponse("login", "user1");
+  await client2.requestResponse("login", "user2");
 
   await messagesExamples(client1, client2);
   await filesExample(client1);
